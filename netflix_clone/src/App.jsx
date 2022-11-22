af066bf4369873b5a79c37cb89fe17c5a4f29b56
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-11-23 00:46:54 +0600

    Completed the setup and routing

---
+++ netflix_clone/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './pages/Login';
+import NetFlix from './pages/NetFlix';
+import SignUp from './pages/SignUp';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="signUp" element={<SignUp />} />
+        <Route exact path="/" element={<NetFlix />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
