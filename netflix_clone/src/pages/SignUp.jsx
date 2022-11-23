834cc3c2b96b8e85bfe574be9771afda8d058b32
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-11-24 00:32:29 +0600

    Update SignUp.jsx

--- netflix_clone/src/pages/SignUp.jsx
+++ netflix_clone/src/pages/SignUp.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
-
+import styled from "styled-components";
+import BackgroundImage from '../components/BackgroundImage';
+import Header from '../components/Header';
 const SignUp = () => {
     return (
-        <div>SignUp</div>
+        <Container>
+            <BackgroundImage />
+            <div className="content">
+                <Header />
+                <div className="body flex column a-center j-center">
+                    <div className="text flex column">
+                        <h1>Unlimited movies, TV shows and more</h1>
+                        <h4>Watch anywhere. Cancel anytime.</h4>
+                        <h6>Ready to watch? Enter your email or restart membership</h6>
+                    </div>
+                    <div className="form">
+                        <input type='email' placeholder='Email Address' name='email' />
+                        <input type='password' placeholder='Password' name='password' />
+                        <button>Get Started</button>
+                    </div>
+                    <button>Log In</button>
+                </div>
+            </div>
+        </Container>
     )
 }
 
+const Container = styled.div`
+position:relative;
+.content{
+    position: absolute;
+    top:0;
+    left:0;
+    background-color:rgba(0,0,0,0.5);
+    height:100vh;
+    width:100vw;
+    dispaly: grid;
+    grid-template-rows:15vh 85vh;
+}
+`;
 export default SignUp;
