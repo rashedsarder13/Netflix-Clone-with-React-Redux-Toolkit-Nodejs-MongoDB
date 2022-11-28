751c4b4a60cd1aeec95c72de616ad04997de415e
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-11-29 00:45:33 +0600

    added new routing for player

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from './pages/Login';
-import NetFlix from './pages/NetFlix';
-import SignUp from './pages/SignUp';
+import Login from "./pages/Login";
+import NetFlix from "./pages/NetFlix";
+import Player from "./pages/Player";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/login" element={<Login />} />
-        <Route exact path="signUp" element={<SignUp />} />
+        <Route exact path="/signUp" element={<SignUp />} />
+        <Route exact path="/player" element={<Player />} />
         <Route exact path="/" element={<NetFlix />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
