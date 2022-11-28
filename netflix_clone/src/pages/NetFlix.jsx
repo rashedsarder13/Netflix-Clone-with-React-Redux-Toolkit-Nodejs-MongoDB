b0a83bf62946d5630fbceed61a476cfe303fc672
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-11-29 00:46:24 +0600

    Update NetFlix.jsx

--- netflix_clone/src/pages/NetFlix.jsx
+++ netflix_clone/src/pages/NetFlix.jsx
@@ -5,9 +5,11 @@ import MovieLogo from "../assets/homeTitle.webp";
 import { FaPlay } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const NetFlix = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const navigate = useNavigate();
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
@@ -27,7 +29,10 @@ const NetFlix = () => {
           <img src={MovieLogo} alt="Movie Logo" />
         </div>
         <div className="buttons flex">
-          <button className="flex j-center a-center">
+          <button
+            className="flex j-center a-center"
+            onClick={() => navigate("/player")}
+          >
             <FaPlay /> Play
           </button>
           <button className="flex j-center a-center">
