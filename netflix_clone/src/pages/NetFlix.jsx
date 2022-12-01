0b29a4b1aec8fc9ef3c9317612ba06a0b8276a19
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-12-02 00:12:54 +0600

    Setup slider component

--- netflix_clone/src/pages/NetFlix.jsx
+++ netflix_clone/src/pages/NetFlix.jsx
@@ -8,11 +8,13 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies, getGenres } from "../store";
+import Slider from "../components/Slider";
 
 const NetFlix = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const navigate = useNavigate();
   const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+  const movies = useSelector((state) => state.netflix.movies);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getGenres());
@@ -52,6 +54,7 @@ const NetFlix = () => {
           </button>
         </div>
       </div>
+      <Slider movies={movies} />
     </Container>
   );
 };
