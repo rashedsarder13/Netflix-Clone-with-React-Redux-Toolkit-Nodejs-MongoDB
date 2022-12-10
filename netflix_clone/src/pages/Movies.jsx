946623cd08b855ef1c3c9a8f096150b4c70300aa
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-12-10 23:55:24 +0600

    Initialize movies page

---
+++ netflix_clone/src/pages/Movies.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchMovies, getGenres } from "../store";
+
+export default function Movies() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const navigate = useNavigate();
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+  const movies = useSelector((state) => state.netflix.movies);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
+  useEffect(() => {
+    if (genresLoaded) {
+      dispatch(fetchMovies({ type: "movies" }));
+    }
+  });
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  return <div>Movies</div>;
+}
