e4f55fbbe5c373b349c52b2b1915e212385a4b8d
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-12-10 23:54:52 +0600

    updating routing

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Movies from "./pages/Movies";
 import NetFlix from "./pages/NetFlix";
 import Player from "./pages/Player";
 import SignUp from "./pages/SignUp";
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signUp" element={<SignUp />} />
         <Route exact path="/player" element={<Player />} />
+        <Route exact path="/movies" element={<Movies />} />
         <Route exact path="/" element={<NetFlix />} />
       </Routes>
     </Router>
