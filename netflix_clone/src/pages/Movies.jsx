5437956936811f669a59d30acdeadcec8b5187df
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-12-11 00:52:19 +0600

    Update Movies.jsx

--- netflix_clone/src/pages/Movies.jsx
+++ netflix_clone/src/pages/Movies.jsx
@@ -1,7 +1,13 @@
+import { onAuthStateChanged } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import NotAvailable from "../components/NotAvailable";
+import Slider from "../components/Slider";
 import { fetchMovies, getGenres } from "../store";
+import { firebaseAuth } from "../utils/firebase-config";
 
 export default function Movies() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -14,12 +20,37 @@ export default function Movies() {
   }, []);
   useEffect(() => {
     if (genresLoaded) {
-      dispatch(fetchMovies({ type: "movies" }));
+      dispatch(fetchMovies({ type: "all" }));
     }
   });
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
   };
-  return <div>Movies</div>;
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) {
+      navigate("/");
+    }
+  });
+  return (
+    <Container>
+      <div className="navbar">
+        <Navbar isScrolled={isScrolled} />
+      </div>
+      <div className="data">
+        {movies.length ? <Slider movies={movies} /> : <NotAvailable />}
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  .data {
+    margin-top: 8rem;
+    .not-available {
+      text-align: center;
+      color: white;
+      margin-top: 4rem;
+    }
+  }
+`;
