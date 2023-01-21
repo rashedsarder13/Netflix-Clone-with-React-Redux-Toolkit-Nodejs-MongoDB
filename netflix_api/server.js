5ed1c81e854367f9957720586bd7185c5b890a27
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2023-01-21 22:13:23 +0600

    Update server.js

--- netflix_api/server.js
+++ netflix_api/server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-const { default: mongoose } = require("mongoose");
 const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/netflix", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose
+    .connect("mongodb://localhost:27017/netflix", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
-        console.log("DB Connected!");
+        console.log("DB Connetion Successfull");
+    })
+    .catch((err) => {
+        console.log(err.message);
     });
 
-app.use("api/user", userRoutes);
+app.use("/api/user", userRoutes);
 
-app.listen(5000, console.log("server started"));
+app.listen(5000, () => {
+    console.log("server started on port 5000");
+});
